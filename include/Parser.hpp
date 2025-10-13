6db5e80e3936d74a42352594074c459c85d5b5b9
TOmorrowarc1 <TOmorrow_arc1@sjtu.edu.cn>
2025-10-13 13:06:05 +0800

    feat: impl Parser module

--- include/Parser.hpp
+++ include/Parser.hpp
@@ -15,18 +15,26 @@ struct ParsedLine {
 
 class Parser {
 public:
-  ParsedLine parseLine(TokenStream &tokens) const;
+  ParsedLine parseLine(TokenStream &tokens,
+                       const std::string &originLine) const;
 
 private:
   std::unique_ptr<Statement>
   parseStatement(TokenStream &tokens, const std::string &originLine) const;
-  std::unique_ptr<Statement> parseLet(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parsePrint(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parseInput(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parseGoto(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parseIf(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parseRem(TokenStream &tokens) const;
-  std::unique_ptr<Statement> parseEnd(TokenStream &tokens) const;
+  std::unique_ptr<Statement> parseLet(TokenStream &tokens,
+                                      const std::string &originLine) const;
+  std::unique_ptr<Statement> parsePrint(TokenStream &tokens,
+                                        const std::string &originLine) const;
+  std::unique_ptr<Statement> parseInput(TokenStream &tokens,
+                                        const std::string &originLine) const;
+  std::unique_ptr<Statement> parseGoto(TokenStream &tokens,
+                                       const std::string &originLine) const;
+  std::unique_ptr<Statement> parseIf(TokenStream &tokens,
+                                     const std::string &originLine) const;
+  std::unique_ptr<Statement> parseRem(TokenStream &tokens,
+                                      const std::string &originLine) const;
+  std::unique_ptr<Statement> parseEnd(TokenStream &tokens,
+                                      const std::string &originLine) const;
 
   std::unique_ptr<Expression> parseExpression(TokenStream &tokens) const;
   std::unique_ptr<Expression> parseExpression(TokenStream &tokens,
@@ -35,5 +43,5 @@ private:
   int getPrecedence(TokenType op) const;
   int parseLiteral(const Token *token) const;
 
-  int leftParentCount;
+  mutable int leftParentCount{0};
 };
